59a6ead544a358e9d97fa9d4e7c33dd132e0e5ca
Tom Wilson <tom@hyper.io>
2022-12-17 11:24:57 +0000

    publish to arweave

---
+++ publish.js
@@ -0,0 +1,17 @@
+import Arweave from 'arweave'
+import Bundlr from '@bundlr-network/client'
+import fs from 'fs'
+
+const arweave = Arweave.init({ host: 'arweave.net', port: 443, protocol: 'https' })
+
+const wallet = JSON.parse(fs.readFileSync('wallet.json', 'utf-8'))
+
+const bundlr = new Bundlr.default("https://node2.bundlr.network", "arweave", wallet)
+
+const data = fs.readFileSync('twilson63-asset-sdk-0.0.1.tgz')
+const tags = [{ name: 'Content-Type', value: 'application/gzip' }]
+
+const tx = bundlr.createTransaction(data, { tags })
+await tx.sign()
+console.log('tx: ', tx.id)
+await tx.upload()
