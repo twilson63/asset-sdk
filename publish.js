f4cabd8a9dfe983ca78161d522f5ac6895c03588
Tom Wilson <tom@hyper.io>
2022-12-17 13:33:19 +0000

    working get asset

--- publish.js
+++ publish.js
@@ -2,16 +2,19 @@ import Arweave from 'arweave'
 import Bundlr from '@bundlr-network/client'
 import fs from 'fs'
 
+const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'))
+const version = pkg.version
 const arweave = Arweave.init({ host: 'arweave.net', port: 443, protocol: 'https' })
 
 const wallet = JSON.parse(fs.readFileSync('wallet.json', 'utf-8'))
 
 const bundlr = new Bundlr.default("https://node2.bundlr.network", "arweave", wallet)
 
-const data = fs.readFileSync('twilson63-asset-sdk-0.0.5.tgz')
+const data = fs.readFileSync(`twilson63-asset-sdk-${version}.tgz`)
 const tags = [{ name: 'Content-Type', value: 'application/gzip' }]
 
 const tx = bundlr.createTransaction(data, { tags })
 await tx.sign()
 console.log('tx: ', tx.id)
 await tx.upload()
+
--- src/services/asset-svc.js
+++ src/services/asset-svc.js
@@ -53,9 +53,39 @@ export default function (env) {
     return { id, ...res }
   }
 
+  function run({ query, variable }) {
+    return fetch(`${URL}/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query, variables })
+    }).then(res => res.ok ? res.json() : Promise.reject(res))
+  }
+
+  async function gql(q) {
+    let hasNextPage = true;
+    let edges = []
+    let cursor = ""
+
+    while (hasNextPage) {
+      const result = await run({ query: q.query, variables: { ...q.variables, cursor } })
+        .then(path(['data', 'transactions']))
+
+      if (result.edges && result.edges.length) {
+        edges = edges.concat(result.edges)
+        cursor = result.edges[result.edges.length - 1].cursor
+      }
+      hasNextPage = result.pageInfo.hasNextPage
+    }
+
+    return edges
+  }
+
   return {
     publish,
-    getData
+    getData,
+    gql
   }
 
 }
